43ff8ad0aeff6c6aae7d4a76ac5d4b840abd9079
Michael Lee <michaelsoolee@gmail.com>
2019-09-13 23:15:48 -0400

    Added description and package details

--- mix.exs
+++ mix.exs
@@ -8,7 +8,10 @@ defmodule ISO639.MixProject do
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      name: "ISO639"
+      name: "ISO639",
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/michaellee/elixir-iso639"
     ]
   end
 
@@ -24,6 +27,23 @@ defmodule ISO639.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    """
+    An Elixir library for working with languages and language codes as defined by the ISO 639 set of standards.
+    """
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/michaellee/elixir-iso639"
+        },
+      maintainers: ["Michael Lee"],
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md", ".formatter.exs"]
     ]
   end
 end
